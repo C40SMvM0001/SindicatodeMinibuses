6ee599f4190784c1d04e7a1fac19b7dbd446bb2b
C-40S_MvM_001 <b377j9123jaja007@gmail.com>
2019-11-18 09:30:59 +0100

    login

--- app/src/main/java/com/android/firebase/sindicatodeminibuses/MainActivity.java
+++ app/src/main/java/com/android/firebase/sindicatodeminibuses/MainActivity.java
@@ -1,14 +1,78 @@
 package com.android.firebase.sindicatodeminibuses;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText _txtCuenta,_txtClave;
+    Button _btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _txtCuenta = findViewById(R.id.txtCuenta);
+        _txtClave = findViewById(R.id.txtClave);
+        _btnIngresar = findViewById(R.id.btnIngresar);
+
+        _btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validarUsuario("http://127.0.0.1/android/validar_usuario.php");
+            }
+        });
+    }
+
+    private void validarUsuario(String URL){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()){
+                    Intent intent = new Intent(getApplicationContext(),PlanillaDeControl.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this,"datos incorrectos",Toast.LENGTH_SHORT);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_SHORT);
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String,String>();
+                parametros.put("Cuenta",_txtCuenta.getText().toString());
+                parametros.put("Clave",_txtClave.getText().toString());
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
